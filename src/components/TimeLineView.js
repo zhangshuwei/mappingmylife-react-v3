435fb8d8d038aa1b4f067959677e98ccd09a1c5c
Zhang Shuwei <swzhang296@gmail.com>
2017-06-29 11:45:22 +0200

    modify timelineView style to make button groups responsive

--- src/components/TimeLineView.js
+++ src/components/TimeLineView.js
@@ -11,12 +11,16 @@ class TimeLineView extends Component {
           <h5>Date de début: <span>{date.start}</span></h5>
           <h5>Date de fin: <span>{date.end}</span></h5>
         </div>
-        <ButtonGroup bsSize='small' className='pull-right'>
-          <Button bsStyle='success' onClick={zoomInTimeLine}><i className='fa fa-plus' aria-hidden='true' /></Button>
-          <Button bsStyle='success' onClick={zoomOutTimeLine}><i className='fa fa-minus' aria-hidden='true' /></Button>
-          <Button bsStyle='success' onClick={moveLeftTimeLine}><i className='fa fa-chevron-left' aria-hidden='true' /></Button>
-          <Button bsStyle='success' onClick={moveRightTimeLine}><i className='fa fa-chevron-right' aria-hidden='true' /></Button>
-        </ButtonGroup>
+        <Row>
+          <Col sm={12}>
+          <ButtonGroup bsSize='small' className='pull-right'>
+            <Button bsStyle='success' onClick={zoomInTimeLine}><i className='fa fa-plus' aria-hidden='true' /></Button>
+            <Button bsStyle='success' onClick={zoomOutTimeLine}><i className='fa fa-minus' aria-hidden='true' /></Button>
+            <Button bsStyle='success' onClick={moveLeftTimeLine}><i className='fa fa-chevron-left' aria-hidden='true' /></Button>
+            <Button bsStyle='success' onClick={moveRightTimeLine}><i className='fa fa-chevron-right' aria-hidden='true' /></Button>
+          </ButtonGroup>
+        </Col>
+        </Row>
         <Row>
           <Col sm={12}>
             <TimeLine geolocations={geolocations} phonecalls={phonecalls} date={date} selectDataByDate={selectDataByDate} mango={mango} />
