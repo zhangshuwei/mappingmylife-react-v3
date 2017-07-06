81f6db5c75e71d1251912472a6428f8cc9dccb83
Zhang Shuwei <swzhang296@gmail.com>
2017-07-06 16:06:18 +0200

    modify docType

--- src/actions/date.js
+++ src/actions/date.js
@@ -1,36 +1,36 @@
 /* global cozy */
 
 import { FETCH_GEO_LAST_DAY, FETCH_GEOLOCATIONS_FAILURE, SELECT_DATE } from '../constants/actionTypes'
-import { GEOLOCATION_DOCTYPE } from '../constants/config'
+// import { GEOLOCATION_DOCTYPE } from '../constants/config'
 import { fetchGeolocations } from './geolocations'
 import { fetchPhonecalls } from './phonecalls'
 
-export const fetchGeoLastDay = (geoIndexByDate) => {
-  return async dispatch => {
-    const options = {
-      'selector': {
-        'docType': GEOLOCATION_DOCTYPE
-      },
-      'fields': ['_id', 'timestamp'],
-      'descending': true,
-      'limit': 1
-    }
-    return cozy.client.data.query(geoIndexByDate, options)
-    .then((date) => {
-      dispatch({
-        type: FETCH_GEO_LAST_DAY,
-        date: date[0].timestamp.slice(0, 10)
-      })
-      return date
-    })
-    .catch((error) => {
-      dispatch({
-        type: FETCH_GEOLOCATIONS_FAILURE,
-        error
-      })
-    })
-  }
-}
+// export const fetchGeoLastDay = (geoIndexByDate) => {
+//   return async dispatch => {
+//     const options = {
+//       'selector': {
+//         'docType': GEOLOCATION_DOCTYPE
+//       },
+//       'fields': ['_id', 'timestamp'],
+//       'descending': true,
+//       'limit': 1
+//     }
+//     return cozy.client.data.query(geoIndexByDate, options)
+//     .then((date) => {
+//       dispatch({
+//         type: FETCH_GEO_LAST_DAY,
+//         date: date[0].timestamp.slice(0, 10)
+//       })
+//       return date
+//     })
+//     .catch((error) => {
+//       dispatch({
+//         type: FETCH_GEOLOCATIONS_FAILURE,
+//         error
+//       })
+//     })
+//   }
+// }
 
 export const selectDataByDate = (start, end, geoIndexByDate, phoneIndexByDate) => {
   return async dispatch => {
